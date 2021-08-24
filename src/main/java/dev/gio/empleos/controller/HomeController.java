eb1f0ea7e1106e9d5f2d5d0e3d990c02520fd6b6
gioslayer <giokreator@gmail.com>
2021-08-24 02:15:51 -0500

    Primer commit. Se agregan los archivos de la clase 12 <<Configurar la página de inicio de una aplicación web>>

---
+++ src/main/java/dev/gio/empleos/controller/HomeController.java
@@ -0,0 +1,13 @@
+package dev.gio.empleos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String mostrarHome() {
+		return "home";
+	}
+} 
