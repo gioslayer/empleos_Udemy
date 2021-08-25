1fec39377cde85b53b9c8599dfa091b81c34ace5
gioslayer <giokreator@gmail.com>
2021-08-25 01:30:09 -0500

    Clase 14 Agregar información al modelo para desplegarla en la vista

--- src/main/java/dev/gio/empleos/controller/HomeController.java
+++ src/main/java/dev/gio/empleos/controller/HomeController.java
@@ -1,13 +1,18 @@
 package dev.gio.empleos.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String mostrarHome() {
+	public String mostrarHome(Model model) {
+		model.addAttribute("mensaje", "Bienvenidos a mi Aplicación de Empleos");
+		model.addAttribute("fecha", new Date());
 		return "home";
 	}
 } 
