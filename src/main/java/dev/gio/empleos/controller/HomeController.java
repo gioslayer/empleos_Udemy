465211979479d6ead16a8446c7c1080fb8818c6b
gioslayer <giokreator@gmail.com>
2021-08-26 00:49:21 -0500

    Clase 16 - Agregar tipos de datos simples al modelo y desplegarlos en la vista

--- src/main/java/dev/gio/empleos/controller/HomeController.java
+++ src/main/java/dev/gio/empleos/controller/HomeController.java
@@ -11,8 +11,20 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
+		/*
 		model.addAttribute("mensaje", "Bienvenidos a mi Aplicación de Empleos");
 		model.addAttribute("fecha", new Date());
+		*/
+		String nombre = "Programador Full Stack";
+		Date fecha = new Date();
+		double salario = 18000;
+		boolean vigente = true;
+		
+		model.addAttribute("nombre", nombre);
+		model.addAttribute("fecha", fecha);
+		model.addAttribute("salario", salario);
+		model.addAttribute("vigente", vigente);
+		
 		return "home";
 	}
 } 
