827060c4d46fc6aaab37d143f10b3c8f7b7fc900
gioslayer <giokreator@gmail.com>
2021-08-28 01:49:57 -0500

    Clase 19 agregar un objeto Vacante y representar una oferta de trabajo en ventana detalle

--- src/main/java/dev/gio/empleos/controller/HomeController.java
+++ src/main/java/dev/gio/empleos/controller/HomeController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import dev.gio.empleos.model.Vacante;
+
 @Controller
 public class HomeController {
 	
@@ -41,5 +43,16 @@ public class HomeController {
 		model.addAttribute("empleos", lista);
 		
 		return "listado";
+	}
+	
+	@GetMapping("/detalle")
+	public String mostrarDetalle(Model model) {
+		Vacante vacante = new Vacante();
+		vacante.setNombre("Ingeniero en Comunicaciones");
+		vacante.setDescripcion("Se solicita ingeniero para dar soporte a intranet");
+		vacante.setFecha(new Date());
+		vacante.setSalario(18000.0);
+		model.addAttribute(vacante);
+		return "detalle";
 	}
 } 
