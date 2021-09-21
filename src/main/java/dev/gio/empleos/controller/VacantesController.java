4117125b8dd16d878e579c415c5171eac224dad3
gioslayer <giokreator@gmail.com>
2021-09-21 02:02:23 -0500

    Clase 48. Formulario para crear una vacante.

--- src/main/java/dev/gio/empleos/controller/VacantesController.java
+++ src/main/java/dev/gio/empleos/controller/VacantesController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -33,5 +34,28 @@ public class VacantesController {
 		System.out.println("Borrando la vacante con el id: " + idVacante);
 		model.addAttribute("idVacante", idVacante);
 		return "mensaje";
+	}
+	
+	@GetMapping("/create")
+	public String crear() {
+		return "vacantes/formVacante";
+	}
+	
+	@PostMapping("/save")
+	public String guardar(@RequestParam("nombre") String nombre,
+			@RequestParam("descripcion") String descripcion,
+			@RequestParam("estatus") String estatus,
+			@RequestParam("fecha") String fecha,
+			@RequestParam("destacado") int destacado,
+			@RequestParam("salario") double salario,
+			@RequestParam("detalles") String detalles) {
+		System.out.println("Nombre de vacante: " + nombre);
+		System.out.println("Descripción: " + descripcion);
+		System.out.println("Estatus: " + estatus);
+		System.out.println("Fecha de Publicación: " + fecha);
+		System.out.println("Destacado: " + destacado);
+		System.out.println("Salario Ofrecido: " + salario);
+		System.out.println("Detalles: " + detalles);
+		return "vacantes/listVacantes";
 	}
 }
