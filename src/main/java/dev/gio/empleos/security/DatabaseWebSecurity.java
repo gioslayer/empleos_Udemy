5fa193dfa5314605a015760ac79dd99efe24867a
gioslayer <giokreator@gmail.com>
2022-06-17 02:04:25 -0500

    Clase 139. Recuperar usuarios y roles desde la base de datos personalizada

--- src/main/java/dev/gio/empleos/security/DatabaseWebSecurity.java
+++ src/main/java/dev/gio/empleos/security/DatabaseWebSecurity.java
@@ -17,6 +17,11 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource);
+		auth.jdbcAuthentication().dataSource(dataSource)
+		.usersByUsernameQuery("select username, password, estatus from usuarios where username=?")
+		.authoritiesByUsernameQuery("select u.username, p.perfil from usuarioperfil up " +
+				"inner join usuarios u on u.id = up.idUsuario " +
+				"inner join perfiles p on p.id = up.idPerfil " +
+				"where u.username =?");
 	}
 }
