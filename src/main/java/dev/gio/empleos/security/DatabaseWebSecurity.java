b884108d9636206f46c9d07fd1d5ab143375b3bb
gioslayer <giokreator@gmail.com>
2022-06-17 20:50:59 -0500

    Clase 140. Personalizar el acceso a las URLs

--- src/main/java/dev/gio/empleos/security/DatabaseWebSecurity.java
+++ src/main/java/dev/gio/empleos/security/DatabaseWebSecurity.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -23,5 +24,29 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 				"inner join usuarios u on u.id = up.idUsuario " +
 				"inner join perfiles p on p.id = up.idPerfil " +
 				"where u.username =?");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+		.antMatchers(
+		//Los recuersos estaticso no requieren autenticación
+				"/bootstrap/**",
+				"/images/**",
+				"/tinymce/**",
+				"/logos/**").permitAll()
+		
+		//Las vistas públicas no requieren Autorización
+		.antMatchers("/",
+				"/signup",
+				"/search",
+				"/vacantes/view/**").permitAll()
+		
+		//Todas las demas URLs de la Aplicación requieren autenticación
+		.anyRequest().authenticated()
+		
+		//El formulario de Login no requiere autenticación
+		.and().formLogin().permitAll();
+		
 	}
 }
